15fa4bbdbba05b6a5eb5a0762a4a822ece3dddb7
ronco <ronco@costite.com>
2016-02-04 11:34:36 -0700

    Manually construct head component (for fastboot)

--- addon/instance-initializers/head.js
+++ addon/instance-initializers/head.js
@@ -1,9 +1,17 @@
 export function initialize(instance) {
   const container = instance.lookup ? instance : instance.container;
-  const component = container.lookup(
-    'component:head-content'
+  const componentFactory =
+          instance._lookupFactory('component:head-content');
+  const renderer = container.lookup('renderer:-dom');
+  // explicitly set renderer & domhelper in case we're in fastboot
+  const component = componentFactory.create(
+    instance.ownerInjection(),
+    {
+      renderer,
+      _domHelper: renderer._dom
+    }
   );
-  component.appendTo('head');
+  component.appendTo(renderer._dom.document.head);
 }
 
 export default {
