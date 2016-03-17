2070c92949963b0d17357e4cce557ca2a7ca607a
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2016-03-17 08:33:47 -0400

    added option to suppress browser rendering for fastboot

--- app/instance-initializers/browser/head.js
+++ app/instance-initializers/browser/head.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
+import ENV from '../../config/environment';
 
 export function initialize(instance) {
+  if (ENV['ember-cli-head'] && ENV['ember-cli-head']['suppressBrowserRender']) { return true; }
+
   // clear fast booted head (if any)
   Ember.$('meta[name="ember-cli-head-start"]')
     .nextUntil('meta[name="ember-cli-head-end"] ~')
