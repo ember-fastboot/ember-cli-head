7793aad5fdb2e3c831b371e42ed4ba80fc5adf43
Dave Bouwman <dave@davebouwman.com>
2016-11-15 17:26:38 -0700

    added suppressClearFastbootedHead option

--- app/instance-initializers/browser/head.js
+++ app/instance-initializers/browser/head.js
@@ -4,11 +4,15 @@ import ENV from '../../config/environment';
 export function initialize(owner) {
   if (ENV['ember-cli-head'] && ENV['ember-cli-head']['suppressBrowserRender']) { return true; }
 
-  // clear fast booted head (if any)
-  Ember.$('meta[name="ember-cli-head-start"]')
-    .nextUntil('meta[name="ember-cli-head-end"] ~')
-    .addBack()
-    .remove();
+  if (ENV['ember-cli-head'] && ENV['ember-cli-head']['suppressClearFastbootedHead']) {
+    //do nothing...
+  }else{
+    // clear fast booted head (if any)
+    Ember.$('meta[name="ember-cli-head-start"]')
+      .nextUntil('meta[name="ember-cli-head-end"] ~')
+      .addBack()
+      .remove();
+  }
 
   const component = owner.lookup('component:head-layout');
   component.appendTo(document.head);
