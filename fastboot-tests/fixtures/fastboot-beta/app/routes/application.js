f4e7b7b3e06852cc77be7c5ca007a1e094516463
simon <ihmig@kaliber5.de>
2017-04-27 21:31:32 +0200

    Added FastBoot 1.0 tests, fixed Travis

---
+++ fastboot-tests/fixtures/fastboot-beta/app/routes/application.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+const { set } = Ember;
+
+export default Ember.Route.extend({
+  headData: Ember.inject.service(),
+  afterModel() {
+    set(this, 'headData.title', "Hello page");
+  }
+});
