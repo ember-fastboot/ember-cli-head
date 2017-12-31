70fd2f23eb5de4b3898d82aa5bd3a0b7fad1ac4f
Mike North <michael.l.north@gmail.com>
2017-12-30 17:17:46 -0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,12 +2,3 @@ declare module 'ember-cli-head/services/head-data' {
   import Service from '@ember/service';
   export default Service;
 }
-
-declare module 'ember' {
-  import HeadDataService from 'ember-cli-head/services/head-data';
-  namespace Ember {
-    interface Route {
-      headData: HeadDataService;
-    }
-  }
-}
