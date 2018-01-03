bfb802e9bc63625fd0a294b18c64fc49487d2c7e
Francesco Novy <francesco@cropster.com>
2018-01-03 14:32:51 +0100

    Remove unecessary files & update travis/testing config

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,11 +1,19 @@
-/* eslint-env node */
 module.exports = {
+  useYarn: true,
   scenarios: [
     {
       name: 'ember-lts-2.12',
       npm: {
         devDependencies: {
           'ember-source': '~2.12.0'
+        }
+      }
+    },
+    {
+      name: 'ember-lts-2.16',
+      npm: {
+        devDependencies: {
+          'ember-source': '~2.16.0'
         }
       }
     },
