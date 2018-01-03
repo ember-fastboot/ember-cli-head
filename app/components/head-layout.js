164452e11f19321e2d85ea601c2205ebc926a2e5
Francesco Novy <francesco@cropster.com>
2018-01-03 13:21:46 +0100

    Move instance initializer cleanup to head-layout component
    Previously, any eventual head data built by fastboot was cleaned up in an instance initializer. This was now moved into the head-layout component, where this happens on init.
    
    This fixes an issue where this cleanup happens multiple times when using lazy loading engines.
    
    Additionally, this moves the head-layout component into the addon namespace, which makes it possible to overwrite/extend from it.

--- app/components/head-layout.js
+++ app/components/head-layout.js
@@ -1,11 +1,1 @@
-import Ember from 'ember';
-import layout from 'ember-cli-head/templates/components/head-layout';
-
-export default Ember.Component.extend({
-  tagName: '',
-  headElement: Ember.computed(function() {
-    let documentService = Ember.getOwner(this).lookup('service:-document');
-    return documentService.head;
-  }),
-  layout
-});
+export { default } from 'ember-cli-head/components/head-layout';
