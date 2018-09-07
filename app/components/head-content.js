6c81f01f645bd43ae849d43cccbd9762d34659da
snewcomer <snewcomer24@gmail.com>
2018-09-06 17:23:24 -0700

    restart it

--- app/components/head-content.js
+++ app/components/head-content.js
@@ -1,9 +1,9 @@
-import { inject as service } from '@ember/service';
+import { inject } from '@ember/service';
 import Component from '@ember/component';
 import layout from '../templates/head';
 
 export default Component.extend({
   tagName: '',
-  model: service('head-data'),
+  model: inject('head-data'),
   layout
 });
