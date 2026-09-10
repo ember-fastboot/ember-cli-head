38c616fd03ccf63f511b29c87298fa69ac482811
Chris Manson <chris@manson.ie>
2026-09-10 11:42:57 +0100

    fix lint

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -134,16 +134,16 @@ module.exports = async function () {
           devDependencies: {
             'ember-source': '~6.12.0',
             'ember-cli': '~6.12.0',
-          }
-        }
+          },
+        },
       }),
       embroiderOptimized({
         npm: {
           devDependencies: {
             'ember-source': '~6.12.0',
             'ember-cli': '~6.12.0',
-          }
-        }
+          },
+        },
       }),
       {
         name: 'no-deprecations',
